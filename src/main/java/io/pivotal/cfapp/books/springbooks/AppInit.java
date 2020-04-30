95232b553b7d15fb259416aa5ca879b58c0dde96
Chris Phillipson <fastnsilver@gmail.com>
2020-04-29 18:10:51 -0700

    CfEnv should be declared as a @Bean

--- src/main/java/io/pivotal/cfapp/books/springbooks/AppInit.java
+++ src/main/java/io/pivotal/cfapp/books/springbooks/AppInit.java
@@ -59,19 +59,19 @@ public class AppInit {
   @Profile("!cloud")
   public static class DefaultConfig {
 
-	@Bean
-	ConnectionFactory spannerConnectionFactory(@Value("${spanner.database}") String database) {
-		Assert.notNull(INSTANCE, "Please provide spanner.instance property");
-		Assert.notNull(GCP_PROJECT, "Please provide gcp.project property");
-		ConnectionFactory connectionFactory = ConnectionFactories.get(ConnectionFactoryOptions.builder()
-			.option(Option.valueOf("project"), GCP_PROJECT)
-			.option(DRIVER, DRIVER_NAME)
-			.option(INSTANCE, SPANNER_INSTANCE)
-			.option(DATABASE, database)
-			.build());
+    @Bean
+    ConnectionFactory spannerConnectionFactory(@Value("${spanner.database}") String database) {
+      Assert.notNull(INSTANCE, "Please provide spanner.instance property");
+      Assert.notNull(GCP_PROJECT, "Please provide gcp.project property");
+      ConnectionFactory connectionFactory = ConnectionFactories.get(ConnectionFactoryOptions.builder()
+        .option(Option.valueOf("project"), GCP_PROJECT)
+        .option(DRIVER, DRIVER_NAME)
+        .option(INSTANCE, SPANNER_INSTANCE)
+        .option(DATABASE, database)
+        .build());
 
-		return connectionFactory;
-	}
+      return connectionFactory;
+    }
 
   }
 
@@ -79,21 +79,25 @@ public class AppInit {
   @Profile("cloud")
   public static class CloudConfig {
 
-	@Bean
-	ConnectionFactory spannerConnectionFactory(@Value("${spanner.database}") String database) {
-		CfEnv cfEnv = new CfEnv();
-		List<CfService> spannerServiceInstances = cfEnv.findServicesByTag("gcp","spanner");
-		Assert.isTrue(spannerServiceInstances.size() == 1, "Only one spanner.instance may exist in the same space");
-		CfService spannerServiceInstance = spannerServiceInstances.get(0);
-		ConnectionFactory connectionFactory = ConnectionFactories.get(ConnectionFactoryOptions.builder()
-			.option(Option.valueOf("project"), spannerServiceInstance.getString("ProjectId"))
-			.option(DRIVER, DRIVER_NAME)
-			.option(INSTANCE, spannerServiceInstance.getString("instance_id"))
-			.option(DATABASE, database)
-			.build());
+    @Bean
+    public CfEnv cfEnv() {
+      return new CfEnv();
+    }
 
-		return connectionFactory;
-	}
+    @Bean
+    ConnectionFactory spannerConnectionFactory(@Value("${spanner.database}") String database) {
+      List<CfService> spannerServiceInstances = cfEnv().findServicesByTag("gcp", "spanner");
+      Assert.isTrue(spannerServiceInstances.size() == 1, "Only one spanner.instance may exist in the same space");
+      CfService spannerServiceInstance = spannerServiceInstances.get(0);
+      ConnectionFactory connectionFactory = ConnectionFactories.get(ConnectionFactoryOptions.builder()
+        .option(Option.valueOf("project"), spannerServiceInstance.getString("ProjectId"))
+        .option(DRIVER, DRIVER_NAME)
+        .option(INSTANCE, spannerServiceInstance.getString("instance_id"))
+        .option(DATABASE, database)
+        .build());
+
+      return connectionFactory;
+    }
 
   }
 
